0c8fb0eadc1b1adf29417a0092eb8a1b3e70ab80
Yuri Elias Alcântara <150107636+YuriElias07@users.noreply.github.com>
2024-06-15 14:01:21 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+# PROGRAM OF NUMBER
+
+# num1 = float(input("Digite o primeiro número:"))
+# num2 = float(input("Digite o segundo número:"))
+# num3 = float(input("Digite o terceiro número:"))
+
+# if num1==num2==num3: 
+#     print("Os três números são iguais")
+# else:
+#     maior_numero = max(num1, num2, num3)
+#     print(f"O maior número digitado é: {maior_numero}")
+
+
+
+
+# PROGRAM OF DIGIT
+
+# digit = str(input("Digite qualquer tecla: ")).lower().strip()
+# vowel = "aeiouAEIOU"
+# consonant = "bdfghjklmnpqrstuvxywzBDFGHJKLMNPQRSTUVXYWZ"
+# num = "0123456789"
+
+# if len(digit) > 1:
+#     print("Digite somente um numero")
+# else:
+#     if digit in vowel:
+#         print("O digito é uma vogal: ")
+#     elif digit in consonant:
+#         print("O digito é uma consoante: ")
+#     elif digit in num:
+#         print("O digito é um número: ")
+#     else: 
+#         print(f"O digito é um caracter especial: ")
+
+
+# PROGRAMA CONTADOR DE NUMEROS IMPARES E PARES
+
+# contador = 1
+# pares = 0
+# impares = 0
+
+# while contador <= 10:
+#     num = int(input("Digite um número: "))
+#     contador += 1
+
+#     if num % 2 == 0:
+#         pares += 1
+#     else:
+#         impares += 1
+    
+# print(f"A quantidade de números impares é: {impares}")
+# print(f"A quantidade de números pares é: {pares}")
+
+
+# PROGRAMA SOMA DE NÚMEROS
+
+# contador = 1
+# soma = 0
+
+# while contador <= 5:
+#     num = int(input("Digite um número: "))
+#     contador += 1
+#     soma += num
+
+# print(f"A soma dos números é igual a: {soma}")
+
+
+
+# PROGRAM OF AGE
+
+# contador = 1
+# idades = 0
+
+# while contador <= 6:
+#     idade = int(input(f"Digite uma idade: "))
+#     contador += 1
+#     if idade > idades:
+#         idades = idade
+#     contador =+ 1
+# print(f"O mais velho tem {idades} anos")
